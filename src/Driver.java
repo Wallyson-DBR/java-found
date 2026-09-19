0fc08dbb2738d958028419963e03d5997ede0b8d
Wallyson Nascimento <wanascimento0901@gmail.com>
2026-09-19 13:09:50 -0300

    add JOptionPane exemple

--- src/Driver.java
+++ src/Driver.java
@@ -1,18 +1,31 @@
+import javax.swing.*;
 import java.util.Scanner;
 
 public class Driver {
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
 
+        /*
         //output
         System.out.println("Hello World!");
 
         //input
-        //Para usar os Scanner devemos importar a classe Scanner.
+        //Para usar o Scanner devemos importar a classe Scanner.
         Scanner sc = new Scanner(System.in);
 
         String message = sc.nextLine();
         System.out.println("Mensagem recebida: " + message);
+        */
+
+        //JOption
+
+        //output
+        JOptionPane.showMessageDialog(null, "Olá mundo... Abrindo uma janela");
+
+        //input
+        String message = JOptionPane.showInputDialog(null, "Escreva algo:");
+        JOptionPane.showMessageDialog(null, message);
+
 
     }
 
